7184be8c732a70de04cdda6b671e8feb363fcd6c
codesign-er <772392166@qq.com>
2016-12-15 10:12:09 +0800

    fix bugs

--- popup.js
+++ popup.js
@@ -10,16 +10,16 @@ function httpRequest(url,callback) {
 }
 
 function showWeather(result){
-	//result = JSON.parse(result);
+	result = JSON.parse(result);
 	var list = result.list;
 	var table = "<table><tr><th>日期</th><th>天气</th><th>最低温度</th><th>最高温度</th></tr>";
 	for(var i in list){
 		var d = new Date(list[i].dt*1000);
 		table += "<tr>";
-		table += "<td>"+d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate()+"</td>";
-		table += "<td>"+list[i].weather.description+"</td>";
-		table += "<td>"+Math.round(list[i].temp.min-237.15)+" °C</td>";
-		table += "<td>"+Math.round(list[i].temp.max-237.15)+" °C</td>";
+		table += "<td>"+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate()+"</td>";
+		table += "<td>"+list[i].weather[0].description+"</td>";
+		table += "<td>"+Math.round(list[i].temp.min-273.15)+" °C</td>";
+		table += "<td>"+Math.round(list[i].temp.max-273.15)+" °C</td>";
 		table += "</tr>"
 	}
 	table += "</table>";
@@ -28,5 +28,5 @@ function showWeather(result){
 
 var city = localStorage.city;
 city = city?city:"beijing";
-var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+city;
+var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+city+',china&lang=zh_cn&APPID=65d1bd8ddc51490ee98f0d478e91db85';
 httpRequest(url, showWeather);
